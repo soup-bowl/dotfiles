66c802b38dc85b510f93a15fc02d8901b8964fa5
Casey <code@soupbowl.io>
2022-08-04 21:28:05 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eu;
+
+# Setup ZSH.
+# Based on https://github.com/trumbitta/dotfiles/blob/main/install.sh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
