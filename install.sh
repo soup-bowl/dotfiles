b43f99a2bb444ddbc035293b55d21695539c30ab
soup-bowl <code@soupbowl.io>
2022-08-04 20:59:40 +0000

    Added check for Gitpod.

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 set -eu;
 
+if test ! -e /ide/bin/remote-cli/gp-code || test ! -v GITPOD_REPO_ROOT; then {
+    printf 'error: This script is meant to be run on Gitpod, quitting...\n' && exit 1;
+} fi
+
 # Setup ZSH.
 # Based on https://github.com/trumbitta/dotfiles/blob/main/install.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
