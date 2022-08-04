a58e672c1e38d1bf6a37776a8ce3da3a0aaf30ff
Casey <code@soupbowl.io>
2022-08-04 21:52:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ set -eu;
 
 # Setup ZSH.
 # Based on https://github.com/trumbitta/dotfiles/blob/main/install.sh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
