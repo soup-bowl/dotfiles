cd278f7cbac0b070f6ac8fe8e3a9b5157dfdedc9
Casey <code@soupbowl.io>
2022-08-05 07:51:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,3 @@ if test ! -e /ide/bin/remote-cli/gp-code || test ! -v GITPOD_REPO_ROOT; then {
 # Setup ZSH.
 # Based on https://github.com/trumbitta/dotfiles/blob/main/install.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
-
-# From the Gitpod .bashrc.
-for i in $(ls -A $HOME/.bashrc.d/); do source $HOME/.bashrc.d/$i; done
-
-if [[ -n $SSH_CONNECTION ]]; then cd "/workspace/sparkling-child"; fi
