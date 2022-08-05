5251c6e3149ac0a891c8c02dd47e6614e016ed82
Casey <code@soupbowl.io>
2022-08-05 08:15:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,9 @@ if test ! -e /ide/bin/remote-cli/gp-code || test ! -v GITPOD_REPO_ROOT; then {
 # Based on https://github.com/trumbitta/dotfiles/blob/main/install.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
 
-echo 'for i in $(ls -A $HOME/.bashrc.d/); do source $HOME/.bashrc.d/$i; done' >> $HOME/.zshrc
+tee -a $HOME/.zshrc <<'EOF'
+for i in $(ls -A $HOME/.bashrc.d/); do source $HOME/.bashrc.d/$i; done
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+EOF
