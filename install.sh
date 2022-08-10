626e5c86aa157a8e2645d49768893160858d6d6c
soup-bowl <code@soupbowl.io>
2022-08-10 17:51:14 +0000

    GP alias fix.

--- install.sh
+++ install.sh
@@ -15,4 +15,6 @@ for i in $(ls -A $HOME/.bashrc.d/); do source $HOME/.bashrc.d/$i; done
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+alias gp="gp"
 EOF
