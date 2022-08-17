9533582761306207dc240674ebcdc2aaa3fa9e64
Casey <code@soupbowl.io>
2022-08-17 22:37:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ if test ! -e /ide/bin/remote-cli/gp-code || test ! -v GITPOD_REPO_ROOT; then {
     printf 'error: This script is meant to be run on Gitpod, quitting...\n' && exit 1;
 } fi
 
+# Setup Fish.
+sudo apt-get install -y fish zsh;
+
 # Setup ZSH.
 # Based on https://github.com/trumbitta/dotfiles/blob/main/install.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
