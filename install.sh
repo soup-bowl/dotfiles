7838a17e6d8d0df56d287ae4d4e0a827916cdf7d
Casey LP <code@soupbowl.io>
2022-12-06 20:59:15 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ if test ! -e /ide/bin/remote-cli/gp-code || test ! -v GITPOD_REPO_ROOT; then {
     printf 'error: This script is meant to be run on Gitpod, quitting...\n' && exit 1;
 } fi
 
+# Setup GPG.
+if [[ ! -z $GNUGPG  ]]
+    rm -rf ~/.gnupg;
+    (cd ~ ; echo $GNUGPG | base64 -d | tar --no-same-owner -xzvf -);
+fi
+
 # Setup Fish.
 sudo apt-get install -y fish zsh;
 
