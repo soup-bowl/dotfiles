f0ff85608525035071c7c44fd98954c855805224
Casey LP <code@soupbowl.io>
2022-12-08 23:25:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 set -eu;
 
-if test ! -e /ide/bin/remote-cli/gp-code || test ! -v GITPOD_REPO_ROOT; then {
-    printf 'error: This script is meant to be run on Gitpod, quitting...\n' && exit 1;
-} fi
-
 # Setup GPG.
 if [[ ! -z $GNUGPG  ]]; then
     rm -rf ~/.gnupg;
@@ -20,11 +16,14 @@ sudo apt-get install -y fish zsh;
 # Based on https://github.com/trumbitta/dotfiles/blob/main/install.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
 
-tee -a $HOME/.zshrc <<'EOF'
-for i in $(ls -A $HOME/.bashrc.d/); do source $HOME/.bashrc.d/$i; done
+# Gitpod Only.
+if test ! -e /ide/bin/remote-cli/gp-code || test ! -v GITPOD_REPO_ROOT; then
+    tee -a $HOME/.zshrc <<'EOF'
+    for i in $(ls -A $HOME/.bashrc.d/); do source $HOME/.bashrc.d/$i; done
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-alias gp="gp"
-EOF
+    alias gp="gp"
+    EOF
+fi
