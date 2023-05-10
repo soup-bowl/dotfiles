c0d884ec5a59979fb0c47f15b865b0c71c35f137
Casey LP <code@soupbowl.io>
2023-05-10 20:31:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,18 @@
 
 set -eu;
 
-# Setup GPG.
-if [[ ! -z $GNUGPG  ]]; then
-    rm -rf ~/.gnupg;
-    (cd ~ ; echo $GNUGPG | base64 -d | tar --no-same-owner -xzvf -);
+# Setup SSH commit signing.
+if [[ ! -z $SSHSIGN_PUB  ]]; then
+    mkdir -p $HOME/.sshgitsigning;
+    echo -n "$SSHSIGN_PUB" | base64 --decode > $HOME/.sshgitsigning/id_rsa.pub;
+    echo -n "$SSHSIGN_PRV" | base64 --decode > $HOME/.sshgitsigning/id_rsa;
+    git config --global gpg.format ssh;
+    git config --global user.signingkey "$HOME/.gitsign/id_rsa.pub";
+    git config --global commit.gpgsign true;
+# Setup GPG commit signing.
+elif [[ ! -z $GNUGPG  ]]; then
+    rm -rf $HOME/.gnupg;
+    (cd $HOME ; echo $GNUGPG | base64 -d | tar --no-same-owner -xzvf -);
     git config --global commit.gpgsign true;
 fi
 
